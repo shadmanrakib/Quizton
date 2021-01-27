fa36ba972a736bcbaa2126b31ed7454886ce9fcc
Ryan <rypoon18@gmail.com>
2021-01-26 20:16:29 -0500

    use next/link to link between base pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,29 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
         <ul className="ml-8 list-disc">
-            <li><a href="/register">Register</a></li>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/search">Search</a></li>
-            <li><a href="/createQuiz">Create Quiz</a></li>
+            <li>    
+                <Link href="/login">
+                    <a>Login</a>
+                </Link>
+            </li>
+            <li>    
+                <Link href="/register">
+                    <a>Register</a>
+                </Link>
+            </li>
+            <li>    
+                <Link href="/search">
+                    <a>Search</a>
+                </Link>
+            </li>
+            <li>    
+                <Link href="/createQuiz">
+                    <a>Create Quiz</a>
+                </Link>
+            </li>
         </ul>
     </div>
   )
