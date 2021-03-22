0547f55fdf1ba3c16c51d162bddb83802568629b
OnikuSpedu <srakibnyc1@gmail.com>
2021-03-21 22:02:59 -0400

    Added env variables

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,19 +4,30 @@ const { parseCookies, setCookie, destroyCookie } = require('nookies');
 
 
 export default async (req, res) => {
-  try {
-    // Check if there is a token and if not return undefined.
-    const parsedCookies = parseCookies({ req });
-    const token = await firebaseAdmin.auth().verifyIdToken(parsedCookies["token"]);
+  const parsedCookies = parseCookies({ req });
 
-    // the user is authenticated!
-    const { uid, email } = token;
+  if (parsedCookies["token"]) {
 
-    return res.status(200).send({message: `Your email is ${email} and your UID is ${uid}.`});
-  } catch (err) {
-    // Return undefined if there is no user. You may also send a different status or handle the error in any way that you wish.
-    console.log(err);
-    const result = undefined;
-    return res.status(200).send(result);
+    try {
+      const token = await firebaseAdmin.auth().verifyIdToken(parsedCookies["token"]);
+
+      // the user is authenticated!
+      const { uid, email } = token;
+
+      // TODO: Processing
+
+      return res.status(200).send({ success: true, message: `Your email is ${email} and your UID is ${uid}.` });
+    } catch (err) {
+      // Return undefined if there is no user. You may also send a different status or handle the error in any way that you wish.
+      console.log(err);
+      const result = undefined;
+      return res.status(200).send({ success: false, message: `User Authentication`});
+    }
+
+  } else {
+
+    return res.status(200).send({ success: false, message: `No User Token` });
+
   }
+
 };
