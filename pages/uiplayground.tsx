3a257d7fe2a90f5a2906a4dcef08cba54ac3e7b4
Ryan <rypoon18@gmail.com>
2021-03-25 20:35:17 -0400

    basic button design

--- pages/uiplayground.tsx
+++ pages/uiplayground.tsx
@@ -1,12 +1,14 @@
-import Modal from '../components/testComponents/modal';
+import Modal from '../components/testComponents/Modal';
+import Button from '../components/testComponents/Button/Button';
 
 const UiPlayground: React.FC = () => {
     return (
-        <>
+        <div className="flex flex-col w-1/3">
             <Modal title="test">
                 <p>Hello</p>
             </Modal>
-        </>
+            <Button text="Click me!" />
+        </div>
     )
 }
 
