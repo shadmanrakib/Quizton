f4dec7ef2f2bf473b69db502fa868340c72feb0a
Ryan <rypoon18@gmail.com>
2021-03-25 22:05:05 -0400

    change z-index of modal

--- components/testComponents/Modal.tsx
+++ components/testComponents/Modal.tsx
@@ -10,7 +10,7 @@ const Modal: React.FC<P> = ({children, title}) => {
         <>
             <button className="border border-black p-3" onClick={() => { setOpen(!open) } }>{ open ? 'Close Modal' : 'Open Modal'}</button>
             { open && 
-            <div className="flex fixed top-0 justify-center items-center w-screen h-screen bg-black opacity-70">
+            <div className="flex fixed z-50 top-0 justify-center items-center w-screen h-screen bg-black opacity-70">
                 <div className="w-11/12 md:w-1/2 p-8 rounded-md bg-white text-black">
                     <div className="flex justify-between mb-5 pb-3 border-b border-gray-800">
                         <h1 className="text-2xl">{ title }</h1>
