d68168aa6f96b228c5db8270debc1869fe08c7e7
Ryan <rypoon18@gmail.com>
2021-03-26 21:06:14 -0400

    basic tooltip

--- pages/uiplayground.tsx
+++ pages/uiplayground.tsx
@@ -1,5 +1,6 @@
 import Modal from '../components/UI/Modal';
 import Button from '../components/UI/Button/Button';
+import Tooltip from '../components/UI/Tooltip';
 
 const UiPlayground: React.FC = () => {
     return (
@@ -9,6 +10,7 @@ const UiPlayground: React.FC = () => {
             </Modal>
             <Button onClick={(e) => { }}> Click Me</Button>
             <Button onClick={(e) => { }} style="focus:outline-none rounded-full bg-green-500 text-white shadow-lg text-xl w-16 h-16 h-10 p-3"> + </Button>
+            <Tooltip>This is a tooltip</Tooltip>
         </div>
     )
 }
