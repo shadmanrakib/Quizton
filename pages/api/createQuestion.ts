92f41684c6f1f6ca39152eb8f5c9e7e28a0ae630
OnikuSpedu <srakibnyc1@gmail.com>
2021-03-28 17:58:28 -0400

    Sanitizer working

--- pages/api/createQuestion.ts
+++ pages/api/createQuestion.ts
@@ -6,22 +6,25 @@ import sanitizeHtml from 'sanitize-html';
 import * as quesdom from "../../types/quesdom";
 
 function sanitize(html: string) {
+  const tags = sanitizeHtml.defaults.allowedTags.concat([ 'math', 'maction', 'maligngroup', 'malignmark', 'menclose', 'merror', 'mfenced', 'mfrac', 'mi', 'mlongdiv', 'mmultiscripts', 'mn', 'mo', 'mover', 'mpadded', 'mphantom', 'mroot', 'mrow', 'ms', 'mscarries', 'mscarry', 'msgroup', 'msline', 'mspace', 'msqrt', 'msrow', 'mstack', 'mstyle', 'msub', 'msup', 'msubsup', 'mtable', 'mtd', 'mtext', 'mtr', 'munder', 'munderover', 'semantics', 'annotation', 'annotation-xml' ]);
+  console.log(tags)
+
   const options = {
-    allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'math', 'maction', 'maligngroup', 'malignmark', 'menclose', 'merror', 'mfenced', 'mfrac', 'mi', 'mlongdiv', 'mmultiscripts', 'mn', 'mo', 'mover', 'mpadded', 'mphantom', 'mroot', 'mrow', 'ms', 'mscarries', 'mscarry', 'msgroup', 'msline', 'mspace', 'msqrt', 'msrow', 'mstack', 'mstyle', 'msub', 'msup', 'msubsup', 'mtable', 'mtd', 'mtext', 'mtr', 'munder', 'munderover', 'semantics', 'annotation', 'annotation-xml' ]),
+    allowedTags: tags,
     allowedAttributes: {
       'span': [ 'class', 'contentEditable' ]
     },
     allowedIframeHostnames: ['www.youtube.com']
   }
   //Separate function in case we want to do more processing or use extra features 
-  return sanitizeHtml(html);
+  return sanitizeHtml(html, options);
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const parsedCookies = parseCookies({ req });
   const inputs = JSON.parse(req.body);
 
-  console.log(inputs.question, inputs.explanation);
+  //console.log(inputs.question, inputs.explanation);
 
   if (!parsedCookies["token"])
     return res.status(200).send({ success: false, message: `No User Token` });
