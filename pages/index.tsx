8a9b99ebbc34d567724e6fa7ef4d66b6ff1ebe44
OnikuSpedu <srakibnyc1@gmail.com>
2021-04-01 23:38:54 -0400

    Added link to create page on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React, { useState } from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Fab from "@material-ui/core/Fab/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Link from "next/link";
 
 const index = () => {
+  const [query, setQuery] = useState("");
 
-    const [query, setQuery] = useState('')
+  return (
+    <div className="min-h-screen w-screen relative">
+      <Navbar changeQuery={setQuery} />
+      <main className="bg-gray-500"></main>
+      <Link href="/question/create">
+      <button className="border h-16 w-16 rounded-full absolute bottom-4 right-4">
+        <AddIcon fontSize="large"/>
+      </button>
+      </Link>
+    </div>
+  );
+};
 
-    return (
-        <>
-            <Navbar changeQuery={setQuery} />
-        </>
-    )
-}
-
-export default index
+export default index;
