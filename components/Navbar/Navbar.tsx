7a524583e46297172942c5c6162e9fcb0ba8c773
OnikuSpedu <srakibnyc1@gmail.com>
2021-04-09 18:52:24 -0400

    SSR responsive navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -7,27 +7,98 @@ import AccountMenu from "./AccountMenu";
 import ArrowBackIcon from "@material-ui/icons/ArrowBackRounded";
 import IconButton from "@material-ui/core/IconButton/IconButton";
 import Button from "@material-ui/core/Button/Button";
-import {useRouter}  from 'next/router';
+import { useRouter } from "next/router";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
+import { useMediaQuery } from "react-responsive";
 
 const Navbar = (props) => {
   const user = useUser();
   const [isUsingMobileSearch, setIsUsingMobileSearch] = useState(false);
   const { register, handleSubmit } = useForm();
   const router = useRouter();
-  
+
+  const isSM = useMediaQuery({ minWidth: 640 });
+  const isMD = useMediaQuery({ minWidth: 768 });
+  const isLG = useMediaQuery({ minWidth: 1024 });
+
   const onSubmitHandler = (data) => {
-    console.log(data)
+    console.log(data);
     router.push({
-        pathname: '/',
-        query: { search: data.query },
-    })
-  }
+      pathname: "/",
+      query: { search: data.query },
+    });
+  };
 
   return (
     <nav>
-      {isUsingMobileSearch ? (
+      <div className={"border flex flex-row items-center w-full h-16"}>
+        <div className="flex-none items-center h-10 mx-4">
+          {isLG ? (
+            <Image
+              src="/full-logo.svg"
+              alt="Quizton"
+              width={192}
+              height={36}
+              onClick={() => router.push("/")}
+            />
+          ) : (
+            <Image
+              src="/logo.svg"
+              alt="Quizton"
+              width={36}
+              height={36}
+              onClick={() => router.push("/")}
+            />
+          )}
+        </div>
+
+        {isMD ? (
+          <div className="flex mx-4 items-center flex-grow min-w-0">
+            <form
+              className="flex border rounded overflow-hidden h-10 w-full min-w-0 max-w-xl"
+              onSubmit={handleSubmit(onSubmitHandler)}
+            >
+              <input
+                type="text"
+                className="flex-auto min-w-0 focus:outline-none p-2"
+                ref={register}
+                name="query"
+              />
+              <button type="submit" className="border px-3">
+                <SearchIcon fontSize="small" />
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div className="flex justify-end flex-grow">
+            <IconButton
+              aria-label="delete"
+              onClick={() => setIsUsingMobileSearch(true)}
+            >
+              <SearchIcon />
+            </IconButton>
+          </div>
+        )}
+
+        {isMD && (
+          <div className="flex-none mx-4">
+            {!user ? (
+              <>
+                <Link href="/auth/login">
+                  <Button variant="text">Login</Button>
+                </Link>
+                <Link href="/auth/signup">
+                  <Button variant="text">Signup</Button>
+                </Link>
+              </> // Add profile picture
+            ) : (
+              <AccountMenu />
+            )}
+          </div>
+        )}
+      </div>
+      {/* {isUsingMobileSearch ? (
        <div className={"border flex flex-row items-center w-full h-16"}>
           <button
             className="mx-2"
@@ -105,7 +176,7 @@ const Navbar = (props) => {
             )}
           </div>
         </div>
-      )}
+      )} */}
     </nav>
   );
 };
