d5fb93efb2eaebcacd1d89b8dca57955fd8e05ae
OnikuSpedu <srakibnyc1@gmail.com>
2021-04-20 11:42:58 -0400

    Deleted origial create question

--- components/CreateQuestionV2/Form.tsx
+++ components/CreateQuestionV2/Form.tsx
@@ -6,6 +6,7 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandLessOutlined";
 import { TrashIcon } from "@heroicons/react/outline";
 import Tags from "./Tags";
 import Choices from "./Choices";
+import router from "next/router";
 
 const defaultMCChoice = {
   answerChoices: [{ value: "" }, { value: "" }, { value: "" }, { value: "" }],
@@ -15,6 +16,24 @@ const defaultMCChoice = {
   question: "",
   tags: [],
 };
+
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "no-cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "include", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 
 export default function Form() {
   const methods = useForm({ defaultValues: defaultMCChoice });
